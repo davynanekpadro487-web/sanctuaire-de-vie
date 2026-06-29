bedaf185a1342dbd518495e25b6abd2027886c24
unknown <davynanekpadro487@gmail.com>
2026-06-29 21:04:22 +0000

    Auto migrate au démarrage

--- sanctuaire_de_vie/wsgi.py
+++ sanctuaire_de_vie/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for sanctuaire_de_vie project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sanctuaire_de_vie.settings')
+
+django.setup()
+
+# Auto-migrate au démarrage
+from django.core.management import call_command
+call_command('migrate', '--run-syncdb')
 
 application = get_wsgi_application()

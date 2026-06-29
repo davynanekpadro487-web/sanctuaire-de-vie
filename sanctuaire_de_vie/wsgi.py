80dee9221b0f83f34b26e56f0233cd74ee33b4ab
unknown <davynanekpadro487@gmail.com>
2026-06-29 23:45:22 +0000

    Create superuser on startup

--- sanctuaire_de_vie/wsgi.py
+++ sanctuaire_de_vie/wsgi.py
@@ -10,4 +10,10 @@ django.setup()
 from django.core.management import call_command
 call_command('migrate', '--run-syncdb')
 
+# Création automatique du superuser s'il n'existe pas
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+
 application = get_wsgi_application()

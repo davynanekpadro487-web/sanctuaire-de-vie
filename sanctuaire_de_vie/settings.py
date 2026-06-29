0c99de2418faef08271f2b697dba2230e267f029
unknown <davynanekpadro487@gmail.com>
2026-06-29 20:33:48 +0000

    Fix static files images

--- sanctuaire_de_vie/settings.py
+++ sanctuaire_de_vie/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 import os
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'sanctuaire/static')]
+STATICFILES_DIRS = [BASE_DIR / 'sanctuaire' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
